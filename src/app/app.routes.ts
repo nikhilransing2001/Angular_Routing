434c981054984164f9ad2b2a020c3c86fe193330
nikhilransing2001 <nikhilransing.scoe.it@gmail.com>
2024-02-01 15:35:05 +0530

    Angular Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { FeaturesComponent } from './admin/features/features.component';
+import { PricingComponent } from './admin/pricing/pricing.component';
+import { HomeComponent } from './admin/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'features',component:FeaturesComponent},
+    {path:'pricing',component:PricingComponent},
+    {path:' ',component:HomeComponent}
+];
